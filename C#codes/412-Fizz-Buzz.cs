34f50a805acff32f57c4dd3b19896bf9fb87bfd6
Pratyush Mohanty <106448796+pratyush532@users.noreply.github.com>
2025-02-18 13:26:26 +0900

    Create 412-Fizz-Buzz.cs

---
+++ C#codes/412-Fizz-Buzz.cs
@@ -0,0 +1,40 @@
+/*Given an integer n, return a string array answer (1-indexed) where:
+
+answer[i] == "FizzBuzz" if i is divisible by 3 and 5.
+answer[i] == "Fizz" if i is divisible by 3.
+answer[i] == "Buzz" if i is divisible by 5.
+answer[i] == i (as a string) if none of the above conditions are true.
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["1","2","Fizz"]
+Example 2:
+
+Input: n = 5
+Output: ["1","2","Fizz","4","Buzz"]
+Example 3:
+
+Input: n = 15
+Output: ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+ 
+
+Constraints:
+*/
+
+public class Solution {
+    public IList<string> FizzBuzz(int n) {
+        
+        string[] listnew = new string[n];
+        for (int i = 0; i< n; i++){
+            string s = "";
+            if ((i+1)%15==0) s = "FizzBuzz";
+            else if ((i+1)%3==0) s =  "Fizz";
+            else if ((i+1)%5==0) s="Buzz";
+            else s = s+ (i+1).ToString();
+            listnew[i] = s;
+        }
+        return listnew;
+    }
+}
